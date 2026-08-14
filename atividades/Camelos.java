4e6d050f13bc388cf19d4af81f8625467427ed66
1cafee <yagonoronhachaves@gmail.com>
2026-08-13 22:27:37 -0300

    Add files via upload

---
+++ atividades/Camelos.java
@@ -0,0 +1,14 @@
+public class Camelos {
+    void main() {
+int camelos, old, mid, bot;
+camelos = 36;
+old = (camelos / 2);
+mid = (camelos / 3);
+bot = (camelos / 9 );
+IO.println("O mais velho terá "+old+" camelos.");
+IO.println("O do meio terá "+mid+" camelos");
+IO.println("O mais novo terá "+bot+" camelos");
+IO.println("E no fim ainda sobrará 2 camelos");
+    
+}
+}
