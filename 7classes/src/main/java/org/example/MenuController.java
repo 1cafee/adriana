1abf268b76334a2f1e023d508c05a399dc90218e
1cafee <yagonoronhachaves@gmail.com>
2026-09-15 22:41:54 -0300

    Add files via upload

---
+++ 7classes/src/main/java/org/example/MenuController.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button btn_aluguel;
+
+    @FXML
+    private Button btn_camelos;
+
+    @FXML
+    private Button btn_descontos;
+
+    @FXML
+    private Button btn_multiplo;
+
+    @FXML
+    private Button btn_senha;
+
+    @FXML
+    private Button btn_tempetura;
+
+    @FXML
+    private Button btn_viagem;
+
+    @FXML
+    void aluguel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void camelos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void descontos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiplo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void senha(ActionEvent event) {
+
+    }
+
+    @FXML
+    void temperatura(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viagem(ActionEvent event) {
+
+    }
+
+}
---
+++ 7classes/src/main/java/org/example/MultiploController.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MultiploController {
+
+    @FXML
+    private Button btn_calcular;
+
+    @FXML
+    private Label lbl_resultado;
+
+    @FXML
+    void calcular(ActionEvent event) {
+
+    }
+
+}
